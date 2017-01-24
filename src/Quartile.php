ecd5912581bf153c9ce2de256e79dd6ce22198b9
René de Kat <info@9lives-development.com>
2017-01-24 16:48:53 +0000

    Refactored

--- src/Quartile.php
+++ src/Quartile.php
@@ -4,26 +4,29 @@ namespace PierceMcGeough\phpquartiles;
 
 class Quartile
 {
-    public $scores;
-    public $quartiles;
+    private $scores;
+    
+    private $quartiles;
 
     /**
      * Create a new Quartiles Instance
+     * @param array $scores
      */
-    public function __construct($scores = null)
+    public function __construct(array $scores)
     {
-        if ($this->arrayOnlyContainsNumbers($scores) === true) {
-            $this->scores = $scores;
-            $this->quartiles = $this->getQuartiles($this->scores);
+        if (!$this->arrayOnlyContainsNumbers()) {
+            throw new \Exceptioon('Scores can only contain numbers');
         }
+       
+        $this->scores = $scores;
+        
+        $this->calculateQuartiles();
     }
 
     /**
      * Calculate the quartiles
-     *
-     * @return array
      */
-    public function getQuartiles()
+    private function calculateQuartiles()
     {
         if (count($this->scores)+1 <= 3) {
             return;
@@ -31,42 +34,55 @@ class Quartile
 
         sort($this->scores, SORT_NUMERIC);
 
-        return [
-            'lowest' => $this->getQuartile($this->scores, 0.25),
-            'third'  => $this->getQuartile($this->scores, 0.50),
-            'second' => $this->getQuartile($this->scores, 0.75)
+        $this->quartiles = [
+            'lowest' => $this->getQuartile(0.25),
+            'third'  => $this->getQuartile(0.50),
+            'second' => $this->getQuartile(0.75)
         ];
     }
-
+    
+    /**
+     * Get the quartiles
+     *
+     * @return array|void
+     */
+    public function getQuartiles()
+    {
+        return $this->quartiles;
+    }
 
     /**
      * Use the params to work out the quartile specific to this $array
      *
-     * @param $array
      * @param double $quartilePlace
      * @return float
      */
-    public function getQuartile($array, $quartilePlace)
+    public function getQuartile($quartilePlace)
     {
-        $pos = (count($array) + 1) * $quartilePlace;
+        $pos = (count($this->scores) + 1) * $quartilePlace;
 
         if (fmod($pos, 1) == 0) {
-            return $array[$pos-1];
+            return $this->scores[$pos-1];
         }
 
         $fraction = $pos - floor($pos);
 
-        $lower_num = $array[floor($pos) - 1];
-        $upper_num = $array[ceil($pos) - 1];
+        $lower_num = $this->scores[floor($pos) - 1];
+        $upper_num = $this->scores[ceil($pos) - 1];
 
         $difference = $upper_num - $lower_num;
 
         return round($lower_num + ($difference * $fraction), 2);
     }
 
-    private function arrayOnlyContainsNumbers($scores)
+    /**
+     * Validate that the scores array only contains numbers
+     *
+     * @return boolean
+     */    
+    private function arrayOnlyContainsNumbers()
     {
-        foreach ($scores as $score) {
+        foreach ($this->scores as $score) {
             if (!is_numeric($score)) {
                 return false;
             }
